6637714829cfbf0befb54cceea40d17be32aeae6
Pietro1005 <pietrocorreali@colegiopaulodetarso.com.br>
2026-02-04 16:03:46 +0000

    hello

---
+++ cometario.c
@@ -0,0 +1,13 @@
+​Para que serve #include <stdio.h>​Onde o programa começa ?
+
+ela é feita pra que o programa consiga interagir com o usuario
+e ela começa a ser executado no main.
+
+O que acontece se remover o return 0?
+
+não acontece nada de mais e o programa roda normalmente
+
+​O que acontece se remover o \n
+
+faz com que o conteúdo subsequente seja exibido na mesma linha,
+em vez de pular para a próxima.
